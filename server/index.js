90d365c57a8407a4d3b1f356b7dc7fba9ba07dcf
unknown <daniellamoussoundamouanda@gmail.com>
2026-01-23 12:56:16 +0100

    espace etudiant oky2

--- server/index.js
+++ server/index.js
@@ -34,8 +34,9 @@ const corsOptions = {
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://localhost:5174',
-    'http://127.0.0.1:5174'
-  ],
+    'http://127.0.0.1:5174',
+    process.env.FRONTEND_URL // URL de votre site sur Hostinger
+  ].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
